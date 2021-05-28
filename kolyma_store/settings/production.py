caea14e6eda119d41c1af4b52019e6bd2df4575e
kipyatiln1k <stepana12@gmail.com>
2021-05-28 16:47:28 +0300

    Enable debug to solve problems on server

--- kolyma_store/settings/production.py
+++ kolyma_store/settings/production.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECRET_KEY = 'django-insecure-q3u2!1bckpf(1=ze&i_zf6m@)!kz(8_mr*cpmysd^&treq(=@#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['models-3d-store.herokuapp.com', '.models-3d-store.com']
 
